a09c31e8f20a1f83169ac6e4e5cdd9d4038a4619
Bomi <o_hmylady@naver.com>
2023-01-30 23:39:14 +0900

    Revert "Revert "react ~19강 - 컴포넌트, state, props""
    This reverts commit d8bf4dd2ae8b6fa6b727c5fd98d22c47a73bf6fa.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import TOC from './components/TOC';
+import Content from './components/Content';
+import Subject from './components/Subject';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// component는 자바스크립트가 아님 >> JSX
+class App extends Component {
+  constructor(props){
+    //render보다 먼저 실행
+    //component 초기화용으로 사용
+    //외부에서 알 필요 없는 정보(내부적으로 사용)
+    super(props);
+    //상위 컴포넌트(app)의 정보(state)를 하위 컴포넌트(subject)로(props) 전달할 수 있음
+    //state 값이 변경되면, 해당 state를 가진 컴포넌트(app)의 render 함수가 다시 호출됨 >> 화면이 다시 그려짐 
+    this.state = {
+      mode:'welcome',
+      subject: {title: "WEB", desc: "World Wide Web!"},
+      welcome: {title: "React", desc: "Hello, React!"},
+      contents: [
+        {id:1, title:'HTML', desc: 'HTML...'},
+        {id:2, title:'cs22s', desc: 'css...'},
+        {id:3, title:'javascript', desc: 'javascript...'}
+      ]
+    }
+  }
+  render() {
+    console.log('App render');
+    var _title, _desc = null;
+    if (this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === 'read') {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
+    // props 전달
+    return (
+      <div className="App">
+        <Subject 
+          title={_title} 
+          sub={_desc}></Subject>
+        <TOC data={this.state.contents}></TOC>
+        <Content title="HTML" discription="HTML is HyperText Markup Language."></Content>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App'; // app.js (생략)
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')); // 이 코드를 기반으로 public의 index.html의 body 내부 div를 타고감
 root.render(
   <React.StrictMode>
-    <App />
+    <App /> 
   </React.StrictMode>
 );
+
+// app은 사용자 정의 태그 (상단에 import)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
