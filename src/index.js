d8bf4dd2ae8b6fa6b727c5fd98d22c47a73bf6fa
Bomi <o_hmylady@naver.com>
2023-01-30 23:38:59 +0900

    Revert "react ~19강 - 컴포넌트, state, props"
    This reverts commit 359ba54cbd4aab976cc7502763e6c66794fb3c2a.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App'; // app.js (생략)
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root')); // 이 코드를 기반으로 public의 index.html의 body 내부 div를 타고감
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App /> 
+    <App />
   </React.StrictMode>
 );
-
-// app은 사용자 정의 태그 (상단에 import)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
